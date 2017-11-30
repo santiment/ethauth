4f97e45d014dc9a386e2aaf31e666570be21c341
human <dip@dip-office.net>
2017-11-30 20:20:33 +0100

    [+] comments and docs

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ethauth
+
+you need a metamask installed into your browser.
+
+TESTING:
+
+`npm start` for starting service out of docker.
+then go to http://localhost:3000/login-test and click on login button.
+
+INTEGRATING:
+html delivery should be performed by calling webserver instead of the service. So move correspondent lines there (marked as EXAMPLE code in index.js)
--- index.js
+++ index.js
@@ -28,8 +28,8 @@ function createMessage(q) {
 
 function checkSignature(q){
     const sign = q.sign;
-    const addr = q.addr || '0x612cd1Ec104273f7D3580f4D617b49D360a98Eff';
-    const hash = q.hash || web3.eth.accounts.hashMessage('Please, login as 0x612cd1ec104273f7d3580f4d617b49d360a98eff');
+    const addr = q.addr; // or your address if testing directly, like: || '0x612cd1Ec104273f7D3580f4D617b49D360a98Eff';
+    const hash = q.hash; // or hash of your message if testing, like:  || web3.eth.accounts.hashMessage('Please, login as 0x612cd1ec104273f7d3580f4d617b49d360a98eff');
     const recovered = web3.eth.accounts.recover(hash,sign);
     return {
       addr: addr,
@@ -38,6 +38,9 @@ function checkSignature(q){
     };
 }
 
+//ToDo: this is an test/example code to incorporate into caller's code.
+// remove it from the service after integration is finished.
+// ==== START  ====
 const LOGIN_TEST_HTML = "<html><head><title>login-test</title></head><body>"
           +   "<input type='button' onclick='location.href=\"/auth-start?addr=\"+web3.eth.accounts[0]' value='login'></input>"
           +   "</script>"
@@ -54,6 +57,7 @@ const CALL_SIGNER_HTML = (msg_hex, addr) =>
           +     "})"
           + "</script>"
       + "</body></html>";
+// ==== END  ====
 
 module.exports = async function (request, response) {
     let req = url.parse(request.url,true);
@@ -62,16 +66,24 @@ module.exports = async function (request, response) {
         send(response, 200, createMessage(q));
     } else if (req.pathname=='/auth-start') {
         const m = createMessage(q);
+
+        // PRODUCTION CODE: use next line to return JSON object
+        //send(response, 200, m);
+
+        //EXAMPLE CODE: returns rendered message. remove the next line after integration
         send(response, 200, CALL_SIGNER_HTML(m.hex, m.addr));
-    } else if (req.pathname=='/auth-check') {
-        send(response, 200, checkSignature(q));
-    } else if (req.pathname=='/auth-reject') {
-        send(response, 200, 'Authentication rejected by user for addr '+q.addr);
     } else if (req.pathname=='/san-balance') {
         const san_balance = await SAN_TOKEN.methods.balanceOf(q.addr).call();
         send(response, 200, san_balance);
+    } else if (req.pathname=='/auth-check') {
+        send(response, 200, checkSignature(q));
+//EXAMPLE CODE: move next calls to webserver.
+///=====> EXAMPLE CODE START
+    } else if (req.pathname=='/auth-reject') {
+        send(response, 200, 'Authentication rejected by user for addr '+q.addr);
     } else if (req.pathname=='/login-test') {
         send(response, 200, LOGIN_TEST_HTML);
+///=====> EXAMPLE CODE END
     } else {
         send(response, 404, 'Not found');
     }
